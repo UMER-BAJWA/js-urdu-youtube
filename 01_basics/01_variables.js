003edd1a4b792666a7686210cd17538318a213f5
Umer Ahmed <146728298+UMER-BAJWA@users.noreply.github.com>
2025-08-08 14:31:02 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144132
+let accountEmail="hsfak@gmail.com"
+var accountPassword="1223"
+accountCity="BWP"
+let accountState;
+// accountId=2 //not allowed
+
+accountEmail="yuio@.com"
+accountPassword="4569"
+accountCity="LHR"
+accountState;
+console.log(accountId);
+
+accountState
+console.log([accountId,accountEmail,accountPassword,accountCity,]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // for newer version 
+
+// alert("hello") // we are using nodejs not browser
+console.log(3+2);
+console.log("laova")
+
+let name="ubr"
+let age =10
+let isLoggedIn= false
+let state=null
+
+// number=>2to power53
+// bigint
+// string
+// boolean=> true/false
+// null=>standalone  value
+// undefined=>
+// symbol=> for unique
+// object
+
+console.log(typeof null) //object
+console.log(typeof undefined)//undefined
