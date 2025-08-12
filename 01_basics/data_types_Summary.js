ced80959f7a3e197cdf168bc474a311dacdadb38
Umer Ahmed <146728298+UMER-BAJWA@users.noreply.github.com>
2025-08-12 15:23:12 +0000

    change in location

---
+++ 01_basics/data_types_Summary.js
@@ -0,0 +1,32 @@
+//  #primitive DT
+
+// 7 types : String,Number, Boolean, Null, undefined, Symbol
+
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+console.log(id === anotherId);
+
+
+// Refrence (Non Primitive)
+// Array, Objects, Functions, 
+
+
+const heros =["shaktiman", "naagra", "doga"]
+let myObj =
+{
+    name:"hyy",
+    age: 33,
+}
+const myFunctoin = function() {
+    console.log("Hello World")
+}
+console.log(typeof anotherId);
