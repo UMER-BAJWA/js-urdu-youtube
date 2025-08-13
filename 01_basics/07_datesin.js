1293a7629f67755c0f5f55556cd38bc47f5c263e
Umer Ahmed <146728298+UMER-BAJWA@users.noreply.github.com>
2025-08-13 14:31:15 +0000

    datein

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,17 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocalString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023,0,23,5,3)
+let myCreatedDate = new Date(20230119)
+console.log(myCreatedDate.toLocalString());
+
+
+let myTimeStamp = Date.now()
+console.log(Math.floor()/1000);
+ 
